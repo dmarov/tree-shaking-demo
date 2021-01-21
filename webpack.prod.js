5b772c66c16a41eb5f3df61051c4dcdb100e8280
Dmitry Marov <d.marov94@gmail.com>
2021-01-21 20:57:44 +0300

    works with webpack 5

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,21 +5,21 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                extractComments: false,
-                terserOptions: {
-                    output: {
-                        comments: false,
-                    },
-                    compress: { 
-                        drop_console: true,
-                        drop_debugger: true,
-                    },
-                },
-            }),
-        ],
-    },
+    // optimization: {
+    //     minimize: true,
+    //     minimizer: [
+    //         new TerserPlugin({
+    //             extractComments: false,
+    //             terserOptions: {
+    //                 output: {
+    //                     comments: false,
+    //                 },
+    //                 compress: { 
+    //                     drop_console: true,
+    //                     drop_debugger: true,
+    //                 },
+    //             },
+    //         }),
+    //     ],
+    // },
 });
