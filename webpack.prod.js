5086a0af8e3167cd01cac3b294e24fcee2acc988
Dmitry Marov <d.marov94@gmail.com>
2021-01-21 20:06:32 +0300

    fixed

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,25 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+                terserOptions: {
+                    output: {
+                        comments: false,
+                    },
+                    compress: { 
+                        drop_console: true,
+                        drop_debugger: true,
+                    },
+                },
+            }),
+        ],
+    },
 });
